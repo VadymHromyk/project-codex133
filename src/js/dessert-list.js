6b5bb3dbbad2e038c731f2c81da0b02656218a82
Irina <vyustirina@gmail.com>
2026-05-24 13:35:10 +0300

    add categories and all desserts

--- src/js/dessert-list.js
+++ src/js/dessert-list.js
@@ -1,22 +1,45 @@
-import { getCategories } from './services/api/api';
+import { getCategories, getDesserts } from './services/api/api';
 
 const selectContainer = document.querySelector('.dessert-list-select');
 const categoriesContainer = document.querySelector('.dessert-categories-list');
+const dessertContainer = document.querySelector('.dessert-list');
 
 console.log(selectContainer);
 console.log(categoriesContainer);
-
+let currentPage = 1;
 async function initDessertList() {
   try {
     const categories = await getCategories();
     console.log(categories);
+    console.log(categories);
+
     renderSelect(categories);
     renderCategoriesBtn(categories);
+    const desserts = await getDesserts({
+      page: currentPage,
+      limit: 8,
+    });
+    console.log(desserts);
+
+    renderDesserts(desserts.desserts);
   } catch (error) {
     console.log(error.message);
   }
 }
 initDessertList();
+
+// async function initDessert() {
+//   try {
+//     const desserts = await getDesserts({
+//       page: 1,
+//       limit: 8,
+//     });
+//     console.log(desserts);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
+// initDessert();
 
 function renderSelect(arr) {
   const markup = arr
@@ -30,7 +53,7 @@ function renderCategoriesBtn(arr) {
     .map(
       ({ name, _id }, index) => `
    <label for="${_id}" class="dessert-category-label">
-        <input ${index === 0 ? 'checked' : ''} 
+        <input
         id="${_id}" type="radio" name="category" 
         value="${_id}" class="dessert-category-input" />
         <span class="dessert-category-btn">
@@ -41,4 +64,27 @@ function renderCategoriesBtn(arr) {
     )
     .join('');
   categoriesContainer.insertAdjacentHTML('beforeend', markup);
+}
+
+function renderDesserts(arr) {
+  const markup = arr
+    .map(
+      ({ name, _id, image, price, category, description }) => `
+  <li class="dessert-list-item" data-id="${_id}">
+        <div class="dessert-list-wrapper">
+          <img class="dessert-list-img" src="${image}" alt="${name}">
+        </div>
+        <p class="dessert-list-category">${category.name}</p>
+        <h3 class="dessert-list-product">${name}</h3>
+        <p class="dessert-list-description">${description}</p>
+        <span class="dessert-list-price">${price} грн</span>
+        <button class="dessert-list-btn" type="button" aria-label="dessert list">
+          <svg class="dessert-list-icon" width="24" height="24">
+            <use href="/img/icons.svg#arrow_outward"></use>
+          </svg>
+        </button>
+      </li>`
+    )
+    .join('');
+  dessertContainer.insertAdjacentHTML('beforeend', markup);
 }
