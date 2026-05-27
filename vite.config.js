fe2fbaf87a1168f2326a6ff50ea0dc571289b271
VadymHromyk <hromuk.vadim@gmail.com>
2026-05-27 21:59:39 +0300

    add fix for imgs

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
+    base: '/project-codex133/',
     root: 'src',
     build: {
       sourcemap: true,
