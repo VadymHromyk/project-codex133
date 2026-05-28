b3767bc27146fa02579c5a8e55bb49886cde7655
Rukas Mykhailo <kozirev7733@gmail.com>
2026-05-28 14:11:54 +0200

    change html css becouse i have conflict wis section feedback

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -7,29 +7,48 @@ import 'swiper/css/pagination';
 
 let aboutUsSwiperInstance = null;
 
+// Оптимизация производительности при изменении размера экрана
+function debounce(func, delay = 150) {
+  let timeoutId;
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func.apply(this, args), delay);
+  };
+}
+
 function handleAboutUsSlider() {
   const isDesktopOrTablet = window.innerWidth >= 768;
 
   if (isDesktopOrTablet) {
     if (!aboutUsSwiperInstance) {
-      aboutUsSwiperInstance = new Swiper('.about_us__swiper', {
+      aboutUsSwiperInstance = new Swiper('#about-us .about_us__swiper', {
         modules: [Navigation, Pagination],
         loop: false,
-        slidesPerView: 2,
-        spaceBetween: 24,
         observer: true,
         observeParents: true,
         observeSlideChildren: true,
         watchSlidesProgress: true,
         watchOverflow: true,
         updateOnWindowResize: true,
+
+        breakpoints: {
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+          },
+          1440: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+          },
+        },
+
         navigation: {
-          nextEl: '.about_us__btn--next',
-          prevEl: '.about_us__btn--prev',
+          nextEl: '#about-us .about_us__btn--next',
+          prevEl: '#about-us .about_us__btn--prev',
           disabledClass: 'swiper-button-disabled',
         },
         pagination: {
-          el: '.about_us__pagination',
+          el: '#about-us .about_us__pagination',
           type: 'bullets',
           clickable: true,
           bulletClass: 'swiper-pagination-bullet',
@@ -49,7 +68,7 @@ function handleAboutUsSlider() {
 
 document.addEventListener('DOMContentLoaded', () => {
   handleAboutUsSlider();
-  window.addEventListener('resize', handleAboutUsSlider);
+  window.addEventListener('resize', debounce(handleAboutUsSlider));
 });
 
 setTimeout(handleAboutUsSlider, 100);
